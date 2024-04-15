8e05d76cf70d1fedc85960e32aa35b292dbfee37
Javicorporation <krchipullajavier@gmail.com>
2024-04-15 14:03:25 -0500

    configuration of the class EmailConfi and EmailDTO and creation of Email.html

--- src/main/java/org/project/proyectoemailsmtp/Configuration/EmailConfi.java
+++ src/main/java/org/project/proyectoemailsmtp/Configuration/EmailConfi.java
@@ -1,14 +1,50 @@
 package org.project.proyectoemailsmtp.Configuration;
 
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
 @Configuration
-@PropertySource("classpath: email.properties")
+@PropertySource("classpath:email.properties")
 public class EmailConfi {
+
     @Value("${email.username}")
     private String correo;
 
-    @Value("${eamil.password}")
+    @Value("${email.password}")
     private String password;
+
+
+    private Properties getMailProperties(){
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth","true");
+        properties.put("mail.smtp.starttls"+".enable","true");
+        properties.put("mail.smtp.host"+"smtp","true");
+        properties.put("mail.smtp.port","587");
+        return properties;
+    }
+
+    @Bean
+    public JavaMailSender javaMailSender(){
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setJavaMailProperties(getMailProperties());
+        mailSender.setUsername(correo);
+        mailSender.setPassword(password);
+        return mailSender;
+    }
+
+    @Bean
+    public ResourceLoader resourceLoader(){
+        return  new DefaultResourceLoader();
+    }
+
+
 }
--- src/main/java/org/project/proyectoemailsmtp/Service/Model/EmailDTO.java
+++ src/main/java/org/project/proyectoemailsmtp/Service/Model/EmailDTO.java
@@ -1,4 +1,32 @@
 package org.project.proyectoemailsmtp.Service.Model;
 
 public class EmailDTO {
+
+    private String destinatario;
+    private String asunto;
+    private String getDestinatario;
+
+    public String getDestinatario() {
+        return destinatario;
+    }
+
+    public void setDestinatario(String destinatario) {
+        this.destinatario = destinatario;
+    }
+
+    public String getAsunto() {
+        return asunto;
+    }
+
+    public void setAsunto(String asunto) {
+        this.asunto = asunto;
+    }
+
+    public String getGetDestinatario() {
+        return getDestinatario;
+    }
+
+    public void setGetDestinatario(String getDestinatario) {
+        this.getDestinatario = getDestinatario;
+    }
 }
